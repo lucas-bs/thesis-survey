dcdc8088853c8735b0d304484820ea74b5aedd1f
lucas-bs <146438181+lucas-bs@users.noreply.github.com>
2024-07-17 17:23:44 +0200

    Update config.py
    removed api key

--- config.py
+++ config.py
@@ -12,6 +12,5 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
 
 SECRET_KEY = "d32rsdfn121dmkao11q422da1LLLo"
 
-OPENAI_API_KEY = "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"
 
 OPENAI_MODEL = "gpt-4o"
