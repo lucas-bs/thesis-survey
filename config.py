7b3b6ad8a2e6510a80230ce7cbd1ad8ecf80da7f
lucas-bs <146438181+lucas-bs@users.noreply.github.com>
2024-07-18 17:05:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-# SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
