63dcc2c49f64dd7eadf96d05478f25fecbe655ac
lucas-bs <146438181+lucas-bs@users.noreply.github.com>
2025-01-28 09:46:56 +0000

    Update views.py

--- gpt/views.py
+++ gpt/views.py
@@ -405,7 +405,7 @@ def pr_new():
             "1. How responsible do you feel for the outcomes of the decisions made during the task?",
             "2. To what extent do you feel your decision was influenced by your own judgment?",
             "3. To what extent do you feel your decision was influenced by the tips received during the activity?",
-            "4. I would have made the same decision without the tips."
+            "4. I would have made the same decision without the tips received during the activity."
         ]
         
     """
